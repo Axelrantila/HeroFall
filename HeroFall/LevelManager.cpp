c406814903abe49091be54b74d0870e708442ac6
Alexander Granell <alexandergranell@hotmail.com>
2013-05-23 18:31:34 +0200

    Level

--- HeroFall/LevelManager.cpp
+++ HeroFall/LevelManager.cpp
@@ -14,13 +14,56 @@ LevelManager::LevelManager(sf::View* view, sf::RenderWindow* window)
 
 	m_levelObjects.push_back(new LevelObjectRectangle(0.0f, 1750.0f, 5000.0f, 200.0f, sf::Color::Magenta));
 	m_levelObjects.push_back(new LevelObjectRectangle(4600.0f, 1650.0f, 5200.0f, 200.0f, sf::Color::Magenta));
-	m_levelObjects.push_back(new LevelObjectRectangle(8800.0f, 1470.0f, 9500.0f, 200.0f, sf::Color::Magenta));
-	m_levelObjects.push_back(new LevelObjectRectangle(9650.0f, 1180.0f, 280.0f, 10.0f, sf::Color::Magenta));
-	m_levelObjects.push_back(new LevelObjectRectangle(10450.0f, 1170.0f, 555.0f, 10.0f, sf::Color::Magenta));
-	m_levelObjects.push_back(new LevelObjectRectangle(14700.0f, 1170.0f, 555.0f, 10.0f, sf::Color::Magenta));
-	m_levelObjects.push_back(new LevelObjectRectangle(14975.0f, 1000.0f, 260.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(8800.0f, 1460.0f, 1100.0f, 200.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(9610.0f, 1120.0f, 280.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(10050.0f, 1250.0f, 280.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(10400.0f, 1460.0f, 2420.0f, 200.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(10500.0f, 1060.0f, 555.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(13000.0f, 1580.0f, 280.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(13400.0f, 1650.0f, 280.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(13800.0f, 1500.0f, 280.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(14200.0f, 1460.0f, 4200.0f, 200.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(14660.0f, 1060.0f, 555.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(14940.0f, 720.0f, 260.0f, 10.0f, sf::Color::Magenta));
 
-	m_player = new Player(0.0f, 1000.0f, this);
+	m_levelObjects.push_back(new LevelObjectRectangle(18300.0f, 1670.0f, 1260.0f, 500.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(19560.0f, 2170.0f, 3060.0f, 200.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(21360.0f, 1400.0f, 600.0f, 100.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(22140.0f, 1600.0f, 300.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(22620.0f, 1970.0f, 1400.0f, 300.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(23820.0f, 1770.0f, 200.0f, 200.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(24020.0f, 2270.0f, 5000.0f, 300.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(26250.0f, 2170.0f, 300.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(24340.0f, 1570.0f, 180.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(24520.0f, 1370.0f, 1000.0f, 200.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(24520.0f, 1570.0f, 10.0f, 450.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(25510.0f, 1570.0f, 10.0f, 450.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(25520.0f, 1560.0f, 100.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(25920.0f, 1370.0f, 1000.0f, 200.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(25920.0f, 1770.0f, 10.0f, 250.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(25820.0f, 1770.0f, 100.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(26910.0f, 1570.0f, 10.0f, 450.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(26720.0f, 1170.0f, 160.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(27420.0f, 1370.0f, 1000.0f, 200.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(27420.0f, 1570.0f, 10.0f, 450.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(28410.0f, 1570.0f, 10.0f, 450.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(27420.0f, 970.0f, 500.0f, 100.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(29020.0f, 2070.0f, 2200.0f, 1600.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(28720.0f, 1370.0f, 500.0f, 20.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(29620.0f, 1370.0f, 1000.0f, 200.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(31220.0f, 3670.0f, 4000.0f, 300.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(31920.0f, 2070.0f, 1000.0f, 200.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(33420.0f, 2070.0f, 1000.0f, 200.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(32720.0f, 3470.0f, 200.0f, 200.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(33520.0f, 3490.0f, 200.0f, 180.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(33320.0f, 3290.0f, 200.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(33720.0f, 3070.0f, 200.0f, 600.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(35220.0f, 3970.0f, 200.0f, 300.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(35420.0f, 4260.0f, 400.0f, 10.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(35820.0f, 3670.0f, 1000.0f, 600.0f, sf::Color::Magenta));
+	m_levelObjects.push_back(new LevelObjectRectangle(35820.0f, 3570.0f, 100.0f, 100.0f, sf::Color::Magenta));
+
+	m_player = new Player(14000.0f, 1000.0f, this);
 	m_view->setCenter(m_player->getCenter().x
 		, m_player->getCenter().y - (float)m_window->getSize().y / 3.5f);
 
@@ -28,7 +71,7 @@ LevelManager::LevelManager(sf::View* view, sf::RenderWindow* window)
 
 	sf::Sprite* tempSprite;
 #pragma region LevelForest
-	tempSprite = SpriteSheetLoader::getInstance()->getSprite("ForestSegment", "ForestSegment_1");
+	/*tempSprite = SpriteSheetLoader::getInstance()->getSprite("ForestSegment", "ForestSegment_1");
 	tempSprite->setPosition(0.0f, 0.0f);
 	m_backgroundSprites.push_back(tempSprite);
 	tempSprite = SpriteSheetLoader::getInstance()->getSprite("ForestSegment", "ForestSegment_2");
@@ -39,52 +82,100 @@ LevelManager::LevelManager(sf::View* view, sf::RenderWindow* window)
 	m_backgroundSprites.push_back(tempSprite);
 	tempSprite = SpriteSheetLoader::getInstance()->getSprite("ForestSegment_4", "ForestSegment_4");
 	tempSprite->setPosition(6000.0f, 0.0f);
-	m_backgroundSprites.push_back(tempSprite);
+	m_backgroundSprites.push_back(tempSprite);*/
 #pragma endregion
 #pragma region LevelMountains
-	tempSprite = SpriteSheetLoader::getInstance()->getSprite("MountainSegment_01", "Mountain_01");
+	/*tempSprite = SpriteSheetLoader::getInstance()->getSprite("MountainSegment_01", "Mountain_01");
 	tempSprite->setPosition(8000.0f, 0.0f);
 	m_backgroundSprites.push_back(tempSprite);
 	tempSprite = SpriteSheetLoader::getInstance()->getSprite("MountainSegment_02", "Mountain_02");
 	tempSprite->setPosition(10328.0f, 0.0f);
 	m_backgroundSprites.push_back(tempSprite);
 	tempSprite = SpriteSheetLoader::getInstance()->getSprite("MountainSegment_03", "Mountain_03");
+	tempSprite->setPosition(14131.0f, 20.0f);
+	m_backgroundSprites.push_back(tempSprite);*/
+	/*tempSprite = SpriteSheetLoader::getInstance()->getSprite("MountainSegment_04", "Mountain_04");
 	tempSprite->setPosition(14131.0f, 0.0f);
+	m_backgroundSprites.push_back(tempSprite);*/
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Banner_02", "Banner_02");
+	tempSprite->setPosition(9840.0f, 1210.0f);
 	m_backgroundSprites.push_back(tempSprite);
-	tempSprite = SpriteSheetLoader::getInstance()->getSprite("MountainSegment_04", "Mountain_04");
-	tempSprite->setPosition(16254.0f, 0.0f);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("01Middleground", "Bridge_01_middleground");
+	tempSprite->setPosition(9880.0f, 1370.0f);
 	m_backgroundSprites.push_back(tempSprite);
-	/*tempSprite = SpriteSheetLoader::getInstance()->getSprite("01Middleground", "Bridge_01_middleground");
-	tempSprite->setPosition(9850.0f, 1370.0f);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Banner_02", "Banner_02");
+	tempSprite->setPosition(10480.0f, 1210.0f);
+	m_backgroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Banner_02", "Banner_02");
+	tempSprite->setPosition(12420.0f, 1210.0f);
 	m_backgroundSprites.push_back(tempSprite);
 	tempSprite = SpriteSheetLoader::getInstance()->getSprite("02Middleground", "Bridge_02_middleground");
 	tempSprite->setPosition(12460.0f, 1370.0f);
 	m_backgroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Banner_02", "Banner_02");
+	tempSprite->setPosition(14600.0f, 1210.0f);
+	m_backgroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Banner_02", "Banner_02");
+	tempSprite->setPosition(15600.0f, 1210.0f);
+	m_backgroundSprites.push_back(tempSprite);
 	tempSprite = SpriteSheetLoader::getInstance()->getSprite("03Middleground", "Bridge_03_middleground");
 	tempSprite->setPosition(15640.0f, 1365.0f);
-	m_backgroundSprites.push_back(tempSprite);*/
+	m_backgroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Banner_02", "Banner_02");
+	tempSprite->setPosition(16960.0f, 1210.0f);
+	m_backgroundSprites.push_back(tempSprite);
 #pragma region ForeGround
-	tempSprite = SpriteSheetLoader::getInstance()->getSprite("MountainSegment_01", "ForeGround1");
-	tempSprite->setPosition(8000.0f, 0.0f);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Foreground_01", "Foreground_01");
+	tempSprite->setPosition(7290.0f, 280.0f);
 	m_foregroundSprites.push_back(tempSprite);
-	tempSprite = SpriteSheetLoader::getInstance()->getSprite("MountainSegment_02", "ForeGround2");
-	tempSprite->setPosition(10328.0f, 0.0f);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Foreground_03_2", "Foreground_03_2");
+	tempSprite->setPosition(10050.0f, 1250.0f);
 	m_foregroundSprites.push_back(tempSprite);
-	tempSprite = SpriteSheetLoader::getInstance()->getSprite("MountainSegment_03", "ForeGround3");
-	tempSprite->setPosition(14131.0f, 0.0f);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Foreground_02", "Foreground_02");
+	tempSprite->setPosition(10500.0f, 1060.0f);
 	m_foregroundSprites.push_back(tempSprite);
-	tempSprite = SpriteSheetLoader::getInstance()->getSprite("MountainSegment_04", "ForeGround4");
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Foreground_03_2", "Foreground_03_2");
+	tempSprite->setPosition(13000.0f, 1580.0f);
+	m_foregroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Foreground_03_2", "Foreground_03_2");
+	tempSprite->setPosition(13400.0f, 1650.0f);
+	m_foregroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Foreground_03_2", "Foreground_03_2");
+	tempSprite->setPosition(13800.0f, 1500.0f);
+	m_foregroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Foreground_03", "Foreground_03");
+	tempSprite->setPosition(14660.0f, 640.0f);
+	m_foregroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Foreground_04", "Foreground_04");
 	tempSprite->setPosition(16254.0f, 0.0f);
 	m_foregroundSprites.push_back(tempSprite);
-	/*tempSprite = SpriteSheetLoader::getInstance()->getSprite("03Foreground", "Bridge_03_foreground");
-	tempSprite->setPosition(15640.0f, 1365.0f);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("03Foreground", "Bridge_03_foreground");
+	tempSprite->setPosition(15640.0f, 1430.0f);
 	m_foregroundSprites.push_back(tempSprite);
 	tempSprite = SpriteSheetLoader::getInstance()->getSprite("02Foreground", "Bridge_02_foreground");
-	tempSprite->setPosition(12460.0f, 1370.0f);
+	tempSprite->setPosition(12460.0f, 1430.0f);
 	m_foregroundSprites.push_back(tempSprite);
 	tempSprite = SpriteSheetLoader::getInstance()->getSprite("01Foreground", "Bridge_01_foreground");
-	tempSprite->setPosition(9850.0f, 1370.0f);
-	m_foregroundSprites.push_back(tempSprite);*/
+	tempSprite->setPosition(9880.0f, 1430.0f);
+	m_foregroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Banner_02", "Banner_02");
+	tempSprite->setPosition(9840.0f, 1290.0f);
+	m_foregroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Banner_02", "Banner_02");
+	tempSprite->setPosition(10480.0f, 1290.0f);
+	m_foregroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Banner_02", "Banner_02");
+	tempSprite->setPosition(12420.0f, 1290.0f);
+	m_foregroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Banner_02", "Banner_02");
+	tempSprite->setPosition(14600.0f, 1290.0f);
+	m_foregroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Banner_02", "Banner_02");
+	tempSprite->setPosition(15600.0f, 1290.0f);
+	m_foregroundSprites.push_back(tempSprite);
+	tempSprite = SpriteSheetLoader::getInstance()->getSprite("Banner_02", "Banner_02");
+	tempSprite->setPosition(16960.0f, 1290.0f);
+	m_foregroundSprites.push_back(tempSprite);
 #pragma endregion
 
 	/*d_bg = SpriteSheetLoader::getInstance()->getSprite("Background", "Background_0");
@@ -97,22 +188,32 @@ LevelManager::LevelManager(sf::View* view, sf::RenderWindow* window)
 
 	m_enemies = new std::vector<Enemy*>();
 	m_enemies->push_back(new EnemyTroll(1800.0f, 1200.0f, m_view));
-	/*m_enemies->push_back(new EnemyTroll(4000.0f, 1300.0f, m_view));
-	m_enemies->push_back(new EnemyShooter(5000.0f, 1250.0f, 100.0f, m_player, m_view));
-	m_enemies->push_back(new EnemyTroll(6200.0f, 1250.0f, m_view));
+	m_enemies->push_back(new EnemyTroll(4000.0f, 1200.0f, m_view));
+	m_enemies->push_back(new EnemyShooter(5000.0f, 1150.0f, 100.0f, m_player, m_view));
+	m_enemies->push_back(new EnemyTroll(6200.0f, 1150.0f, m_view));
 	m_enemies->push_back(new EnemyShooter(6500.0f, 1250.0f, 100.0f, m_player, m_view));
-	m_enemies->push_back(new EnemyTroll(10080.0f, 1080.0f, m_view));
-	m_enemies->push_back(new EnemyShooter(9650.0f, 960.0f, 100.0f, m_player, m_view));
-	m_enemies->push_back(new EnemyShooter(10850.0f, 960.0f, 100.0f, m_player, m_view));
-	m_enemies->push_back(new EnemyTroll(12480.0f, 1080.0f, m_view));
+	m_enemies->push_back(new EnemyShooter(9600.0f, 800.0f, 100.0f, m_player, m_view));
+	m_enemies->push_back(new EnemyShooter(10500.0f, 800.0f, 100.0f, m_player, m_view));
+	m_enemies->push_back(new EnemyTroll(12400.0f, 900.0f, m_view));
 	m_enemies->push_back(new EnemyGoblin(11000.0f, 200.0f, 1500.0f));
-	m_enemies->push_back(new EnemyTroll(13880.0f, 1080.0f, m_view));
-	m_enemies->push_back(new EnemyShooter(14000.0f, 960.0f, 100.0f, m_player, m_view));
-	m_enemies->push_back(new EnemyGoblin(14400.0f, 200.0f, 1500.0f));
-	m_enemies->push_back(new EnemyShooter(14700.0f, 960.0f, 100.0f, m_player, m_view));
-	m_enemies->push_back(new EnemyShooter(15200.0f, 780.0f, 100.0f, m_player, m_view));
-	m_enemies->push_back(new EnemyTroll(17000.0f, 1080.0f, m_view));
-	m_enemies->push_back(new EnemyTroll(17400.0f, 1080.0f, m_view));*/
+	m_enemies->push_back(new EnemyTroll(13900.0f, 900.0f, m_view));
+	m_enemies->push_back(new EnemyShooter(13800.0f, 800.0f, 100.0f, m_player, m_view));
+	m_enemies->push_back(new EnemyGoblin(15200.0f, 0.0f, 1500.0f));
+	m_enemies->push_back(new EnemyShooter(14640.0f, 600.0f, 100.0f, m_player, m_view));
+	m_enemies->push_back(new EnemyShooter(14900.0f, 400.0f, 100.0f, m_player, m_view));
+	m_enemies->push_back(new EnemyTroll(17000.0f, 900.0f, m_view));
+	m_enemies->push_back(new EnemyTroll(17400.0f, 900.0f, m_view));
+	m_enemies->push_back(new EnemyTroll(20600.0f, 1640.0f, m_view));
+	m_enemies->push_back(new EnemyShooter(21320.0f, 1200.0f, 100.0f, m_player, m_view));
+	m_enemies->push_back(new EnemyShooter(21880.0f, 1200.0f, 100.0f, m_player, m_view));
+	m_enemies->push_back(new EnemyGoblin(21500.0f, 0.0f, 1800.0f));
+	m_enemies->push_back(new EnemyTroll(24800.0f, 1700.0f, m_view));
+	m_enemies->push_back(new EnemyShooter(25540.0f, 1300.0f, 100.0f, m_player, m_view));
+	m_enemies->push_back(new EnemyTroll(27600.0f, 1700.0f, m_view));
+	m_enemies->push_back(new EnemyShooter(26680.0f, 1000.0f, 100.0f, m_player, m_view));
+	m_enemies->push_back(new EnemyShooter(27420.0f, 1200.0f, 100.0f, m_player, m_view));
+	m_enemies->push_back(new EnemyShooter(27400.0f, 600.0f, 100.0f, m_player, m_view));
+	m_enemies->push_back(new EnemyShooter(28880.0f, 1200.0f, 100.0f, m_player, m_view));
 
 	for(int a = 0; a < 100; a++)
 	{
@@ -121,6 +222,8 @@ LevelManager::LevelManager(sf::View* view, sf::RenderWindow* window)
 
 	m_popupImages = new PopupImageManager();
 	m_popupImages->addImage("Base", "Base_Walk_0_0", sf::Vector2f(3700.0f, 1300.0f), sf::Vector2f(0.0f, -25.0f), 2.0f);
+
+	std::cout << "TestFinal\n";
 }
 
 LevelManager::~LevelManager()
@@ -155,11 +258,12 @@ void LevelManager::draw(sf::RenderWindow* window)
 	for(unsigned int a = 0; a < m_backgroundSprites.size(); a++)
 		{window->draw(*m_backgroundSprites[a]);}
 		
-	//for(unsigned int a = 0; a < m_levelObjects.size(); a++)
-	//	{m_levelObjects[a]->draw(window);}
+	for(unsigned int a = 0; a < m_levelObjects.size(); a++)
+		{m_levelObjects[a]->draw(window);}
 
 	for(unsigned int a = 0; a < m_enemies->size(); a++)
 		{m_enemies->at(a)->draw(window);}
+
 	m_player->draw(window);
 
 	for(unsigned int a = 0; a < m_foregroundSprites.size(); a++)
